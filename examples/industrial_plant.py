161132148484f87b8604e33476f9d0c37cf8d237
Siwat Sirichai <siwat@siwatinc.com>
2023-12-05 00:39:40 +0700

    Update industrial_plant.py

--- examples/industrial_plant.py
+++ examples/industrial_plant.py
@@ -1,11 +1,43 @@
 from espmega.espmega_r3 import ESPMega_standalone as ESPMega
 from time import sleep as delay
 from time import perf_counter as clock
-plc = ESPMega("/espmega/ProR3", "192.168.0.26", 1883)
+from simple_pid import PID
+from math import log
 
+# Thermistor parameters
+R1 = 50000  # Bias resistor value in ohms
+R2 = 100000  # Thermistor resistance at reference temperature in ohms
+T0 = 298.15  # Reference temperature in Kelvin
+B = 3950  # Beta coefficient of the thermistor
+
+def adc_to_temp(adc_value):
+    # Convert ADC value to voltage
+    voltage = adc_value * (12 / 65535)
+
+    # Convert voltage to resistance using voltage divider formula
+    resistance = (R1 * voltage) / (12 - voltage)
+
+    # Convert resistance to temperature using Steinhart-Hart equation
+    steinhart = (resistance / R2) - 1
+    steinhart = B / steinhart
+    steinhart += 1 / T0
+    steinhart = 1 / steinhart
+    temperature = steinhart - 273.15  # Convert temperature to Celsius
+
+    return temperature
+
+plc = ESPMega("/espmega/ProR3", "192.168.0.26", 1883)
+pid = PID(1, 0.1, 0.05, setpoint=100, sample_time=0.1, output_limits=(0, 4095))
+
+# Digital PWM Outputs
 inlet_valve_pin = 0
 outlet_valve_pin = 1
-boiler_heater_pin = 2
+
+# True Analog Outputs
+boiler_heater_pin = 0
+
+# Analog Inputs
+boiler_temperature_sensor_pin = 0
 
 temp_sensor_pin = 0
 temp_threshold = 120
@@ -13,6 +45,9 @@ temp_threshold = 120
 boil_time = 100
 fill_time = 5
 drain_time = 10
+
+def adc_to_temp(adc_value):
+    
 
 # Boiler plant implementation
 def boiler_plant():
@@ -22,17 +57,18 @@ def boiler_plant():
     delay(fill_time)
     # Close the inlet valve
     plc.digital_write(inlet_valve_pin, 0)
-    # Turn on the boiler heater
-    plc.digital_write(boiler_heater_pin, 1)
     # Start measuring the time
     start_time = clock()
     # Wait until the temperature reaches the threshold or the time is up
     while plc.analog_read(temp_sensor_pin) < temp_threshold:
         if clock() - start_time > boil_time:
             break
-        delay(1)
+        current_temperature = adc_to_temp(plc.analog_read(boiler_temperature_sensor_pin))
+        actuator_value = pid(current_temperature)
+        plc.analog_write(boiler_heater_pin, actuator_value)
+        delay(0.1)
     # Turn off the boiler heater
-    plc.digital_write(boiler_heater_pin, 0)
+    plc.dac_write(boiler_heater_pin, 0)
     # Open the outlet valve
     plc.digital_write(outlet_valve_pin, 1)
     # Start draining the boiler
