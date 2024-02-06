2977420438c0967a071a90effcbdedb1e640540f
Siwat Sirichai <siwat@siwatinc.com>
2024-02-06 21:21:30 +0700

    Update cards.py

--- src/espmega/cards.py
+++ src/espmega/cards.py
@@ -1,7 +1,16 @@
-import abc
-
-class Card(metaclass=abc.ABCMeta):
-    pass
+from abc import ABC, abstractmethod
+from espmega.connection import ESPMegaConnectionManager
+class Card(ABC):
+    card_id: int
+    base_topic: str
+    conn: ESPMegaConnectionManager
+    server: str
+    @abstractmethod
+    def __init__(self, card_id: int, base_topic: str, conn: ESPMegaConnectionManager, server: str):
+        pass
+    @abstractmethod
+    def subscribe_card(self):
+        pass
 
 class DigitalInputCard(Card):
     pass
